d4355834b47a3dccc62939b243f4db6676557499
Sarah Durand <sarahdurand2007@gmail.com>
2025-10-26 15:28:02 +0100

    ajout fonctions

--- operations.h
+++ operations.h
@@ -1,11 +1,13 @@
 #ifndef OPERATIONS_H_INCLUDED
 #define OPERATIONS_H_INCLUDED
 
-void addition(int* a, int b);
-void soustraction(int* a, int b);
-void multiplication(int* a, int b);
-void division(int* a, int* b);
-void ex(int* a);
-void racine(int *a);
+void addition(float* a, float b);
+void soustraction(float* a, float b);
+void multiplication(float* a, float b);
+void division (float*a,float*b);
+void exponential(float* a);
+void racine(float* a);
+void cosinus (float* a, char* angle);
+void sinus (float* a, char* angle);
 
 #endif // OPERATIONS_H_INCLUDED
